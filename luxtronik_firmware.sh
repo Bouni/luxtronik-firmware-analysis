dc74ef894334878f87d3df9df08520fd64a8abfe
Bouni <bouni@owee.de>
2024-02-15 17:12:36 +0100

    automatically extract V1 firmware

--- luxtronik_firmware.sh
+++ luxtronik_firmware.sh
@@ -11,18 +11,17 @@ echo "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$software
 filename=$(curl -s -I "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$softwareID" | grep -oP 'wp2?reg[-.][VF]\d+.\d+.\d+')
 echo "Latest firmware is $filename"
 
-if [[ "$filename" == *"wpreg."*  ]]; then
-    curl -s "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$softwareID" --output "$filename"
-    echo "Downloaded as $filename"
-    echo "Firmware $filename can be decompressed with python via \"./extract_V1_firmware.py $filename\"."
-    exit
+if [[ "$filename" == *"wpreg."* ]]; then
+	curl -s "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$softwareID" --output "$filename"
+	echo "Downloaded as $filename"
+	echo "Decompress downloaded firmware"
+	python extract_V1_firmware.py $filename
+	rm $filename
+	exit
 else
-    curl -s "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$softwareID" --output "$filename.gz"
-    echo "Downloaded as $filename.gz"
+	curl -s "https://www.heatpump24.com/software/fetchSoftware.php?softwareID=$softwareID" --output "$filename.gz"
+	echo "Downloaded as $filename.gz"
 fi
-
-exit 0
-
 
 echo "Decompress downloaded firmware"
 gzip -qdkc "$filename.gz" | tar xf -
@@ -32,9 +31,9 @@ rm "$filename.gz"
 echo "Verify Checksum"
 find . -name 'home.*.md5' -print0 | xargs -0 md5sum -c
 
-if [ $? -ne 0  ]; then
-    echo "Error, checksum does not match!"
-    exit $?
+if [ $? -ne 0 ]; then
+	echo "Error, checksum does not match!"
+	exit $?
 fi
 
 # Delete md5 sum file, not needed anymore
@@ -42,7 +41,7 @@ rm home.*.md5
 
 # Rename tar file to make tar happy
 filename=$(find . -name 'home.wp2reg*')
-mv $filename "$filename.tar" 
+mv $filename "$filename.tar"
 
 # Make a new directory for untared firmware
 mkdir firmware >/dev/null 2>&1
@@ -51,6 +50,4 @@ echo "Extract firmware into ./firmware"
 tar xf "$filename.tar" -C firmware
 
 # Delete tar ball
-rm "$filename.tar" 
-
-
+rm "$filename.tar"
